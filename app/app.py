8fb8237a8484b77300f85f78de1094af780fd917
Alessandro Guaresti <alessguar@gmail.com>
2021-09-20 18:45:36 -0700

    save

--- app/app.py
+++ app/app.py
@@ -1,6 +1,16 @@
+import json
+from flask import request
+from sqlalchemy import text
+from flask_sqlalchemy import SQLAlchemy
+from http import HTTPStatus
+from initialise import Initialise
 from flask import Flask
-
+# Creates the Flask app
 app = Flask(__name__)
+init = Initialise()
+app = init.db(app)
+# Creates the db connection
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
--- app/database/migration.py
+++ app/database/migration.py
@@ -1,17 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
+from flask_migrate import Migrate
+
+import sys
+sys.path.append("..")
+from initialise import Initialise
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@flask-db/users'
+init = Initialise()
+app = init.db(app)
 
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
-
-handler = Manager(app)
-handler.add_command('db', MigrateCommand)
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,4 +21,4 @@ class Users(db.Model):
     identity_number = db.Column(db.Integer())
 
 if __name__ == '__main__':
-    handler.run()
+    app.run()
