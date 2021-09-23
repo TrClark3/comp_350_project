ba6ad9c7d5b0fbcd36c763119ca65b5436b4b157
Alessandro Guaresti <alessguar@gmail.com>
2021-09-22 17:18:36 -0700

    forgot that username is 32 char in this example

--- Tests/resttest.sh
+++ Tests/resttest.sh
@@ -1,6 +1,6 @@
 #! /bin/bash 
 
-curl -H "Content-Type: application/json" -d '{	"username": "suscipit.nonummy@necorcidonec.co.uk",	"password": "KLH24GVU5RE",	"f_name": "Hayfa",	"l_name": "Gray",	"age": 36}' -X POST http://localhost:8000/api/users
+curl -H "Content-Type: application/json" -d '{	"username": "suscipit.nonummy@nenec.co.uk",	"password": "KLH24GVU5RE",	"f_name": "Hayfa",	"l_name": "Gray",	"age": 36}' -X POST http://localhost:8000/api/users
 curl -H "Content-Type: application/json" -d '{	"username": "mi.ac@lobortis.co.uk",	"password": "YWR25ETU2MO",	"f_name": "Athena",	"l_name": "Bentley",	"age": 95}' -X POST http://localhost:8000/api/users
 curl -H "Content-Type: application/json" -d '{	"username": "placerat@augueid.ca",	"password": "QNH11HER6RH",	"f_name": "Paloma",	"l_name": "Potts",	"age": 57}' -X POST http://localhost:8000/api/users
 curl -H "Content-Type: application/json" -d '{	"username": "quis.lectus@ametconsectetuer.net",	"password": "UOU42VFI5YI",	"f_name": "Isaac",	"l_name": "Kane",	"age": 21}' -X POST http://localhost:8000/api/users
