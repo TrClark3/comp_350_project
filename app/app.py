5b9b83e60a7fe68d7a533a138bc9cd05ff110db8
Travis Clark <travis.clark490@myci.csuci.edu>
2021-09-26 09:05:35 -0700

    created template pages (empty), linked stylesheet

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from initialise import Initialise
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_restful import Resource, Api
