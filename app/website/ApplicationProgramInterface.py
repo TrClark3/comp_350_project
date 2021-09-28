53098e5175fa12f5c4b0cb77d7a9920ba02e39e2
Alessandro Guaresti <alessguar@gmail.com>
2021-09-27 17:55:07 -0700

    need a initial users.sql

--- app/website/ApplicationProgramInterface.py
+++ app/website/ApplicationProgramInterface.py
@@ -1,7 +1,7 @@
 from flask import request, jsonify
 from .models import *
 from flask_restful import Resource
-from . import api
+from . import db
 
 
 class UserManager(Resource):
@@ -79,6 +79,3 @@ class UserManager(Resource):
         return jsonify({
             'Message': f'User {str(user_id)} deleted.'
         })
-
-
-api.add_resource(UserManager, '/api/users')
