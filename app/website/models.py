916cd95cdff7ac41066fa23b8ade86bb14ce8c87
Alessandro Guaresti <alessguar@gmail.com>
2021-10-03 13:36:50 -0700

    save. Python has problems structuring db

--- app/website/models.py
+++ app/website/models.py
@@ -1,4 +1,25 @@
+import enum
+
 from website import db, ma
+
+
+# Enumerations
+class PaymentType(enum.Enum):
+    CC = "CC"
+    CASH = "CASH"
+    CHECK = "CHECK"
+
+
+class ServiceType(enum.Enum):
+    SERVICE1 = "SERVICE1"
+    SERVICE2 = "SERVICE2"
+    SERVICE3 = "SERVICE3"
+
+
+class RoomType(enum.Enum):
+    KING = "KING"
+    QUEEN = "QUEEN"
+    JUNIOR = "JUNIOR"
 
 
 # Models
@@ -29,6 +50,55 @@ class Employee(db.Model):
         self.l_name = lastname
 
 
+class Customer(db.Model):
+    cust_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(50))
+    password = db.Column(db.String(50))
+    payment_type = db.Column(db.Enum(PaymentType))
+    payment_info = db.Column(db.String(50))
+
+    def __init__(self, username, password, ptype, pinfo):
+        self.username = username
+        self.password = password
+        self.payment_type = ptype
+        self.payment_info = pinfo
+
+
+class HotelRoom(db.Model):
+    room_num = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    room_type = db.Column(db.Enum(RoomType))
+    smoking = db.Column(db.Boolean)
+
+    def __init__(self, number, type, smoking):
+        self.room_num = number
+        self.room_type = type
+        self.smoking = smoking
+
+
+class HotelReservation(db.Model):
+    res_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    room_num = db.Column(db.Integer)
+    cust_id = db.Column(db.Integer, db.ForeignKey('Customer.cust_id'))
+    check_in = db.Column(db.Date)
+    check_out = db.Column(db.Date)
+
+
+class SpaService(db.Model):
+    service_type = db.Column(db.Enum(ServiceType), primary_key=True)
+    start_time = db.Column(db.Time)
+    end_time = db.Column(db.Time)
+
+
+class SpaReservation(db.Model):
+    res_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    room_num = db.Column(db.Integer, db.ForeignKey('HotelReservation.res_id'))
+    cust_id = db.Column(db.Integer, db.ForeignKey('Customer.cust_id'))
+    spa_start = db.Column(db.Date)
+    start_time = db.Column(db.Time)
+    end_time = db.Column(db.Time)
+    service_type = db.Column(db.Enum(ServiceType), db.ForeignKey('SpaService.service_type'))
+
+
 # Schema
 class UsersSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
@@ -39,6 +109,36 @@ class UsersSchema(ma.SQLAlchemyAutoSchema):
 class EmployeesSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Employee
+        include_fk = True
+
+
+class CustomerSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Customer
+        include_fk = True
+
+
+class HotelRoomSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = HotelRoom
+        include_fk = True
+
+
+class HotelReservationSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = HotelReservation
+        include_fk = True
+
+
+class SpaServiceSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = SpaService
+        include_fk = True
+
+
+class SpaReservationSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = SpaReservation
         include_fk = True
 
 
