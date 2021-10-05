96099cb74c2508fef447532dfb4e66ec7e69f995
Travis Clark <travis.clark490@myci.csuci.edu>
2021-10-05 12:00:23 -0700

    Fixed signUp method in views.py. new user info now sends to db

--- app/website/views.py
+++ app/website/views.py
@@ -7,13 +7,13 @@ import json
 
 views = Blueprint('views', __name__)
 
-
+# Homepage. Uses index.html
+@views.route('/home')
 @views.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-
-# Handle a User Sign-Up
+# SignUp Page. Uses signUp.html
 @views.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     # Handles POST method logic
@@ -25,31 +25,7 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        # validate form data to fit in database. Commented out bc I need to import bootstrap but im too lazy rn
-        # We should also validate the username is unique somehow
-        """
-            Technically since usernames are always unique in our database 
-            we would need to handle the case if the entered username is already 
-            in the database
-                        - Alessasndro
-        """
-
-        # if (len(f_name) < 2 or len(f_name) > 32):
-        #     flash('First Name must be between 2 and 32 characters.', category='error')
-        # elif (len(l_name) < 2 or len(l_name) > 32):
-        #     flash('Last Name must be between 2 and 32 characters.', category='error')
-        # elif (len(username) < 4 or len(username) > 50):
-        #     flash('Username must be between 4 and 50 characters.', category='error')
-        # elif password1 != password2:
-        #     flash('Passwords do not match!', category='error')
-        # elif len(password1) < 5:
-        #     flash('Password must be between 5 and 32 characters.', category='error')
-        # else:
-        #     # add user to database
-        #     flash('Account Created!', category='success')
-
-        # add user to database, encrypt password using werkzeug import
-        new_user = User(username=username, password=generate_password_hash(password1, method='sha256'),
+        new_user = User(username=username, password=password1,
                         first=f_name, last=l_name, age=0)
         db.session.add(new_user)
         db.session.commit()
@@ -57,7 +33,6 @@ def sign_up():
 
     # Render the sign-up page
     return render_template('sign-up.html')
-
 
 @views.route('/thanks')
 def thanks():
