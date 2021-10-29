f62ac0541099d682f8f104fe9b25bafe51c18f32
Alessandro Guaresti <alessguar@gmail.com>
2021-10-28 18:36:44 -0700

    added some dummy data to copypaste into adminer

---
+++ DB/dumydata.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO `hotelroom` (`room_num`, `room_type`, `smoking`) VALUES
+(1,	'KING',	NULL),
+(2,	'QUEEN',	NULL),
+(3,	'JUNIOR',	NULL),
+(4,	'KING',	1),
+(5,	'QUEEN',	1),
+(6,	'JUNIOR',	1);
